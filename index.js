b8876f7f947793228a4038439b87f5c4720d7706
mohannmm <mohannmm@miamioh.edu>
2021-03-21 16:29:01 -0400

    express install and api comments

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+//get//
+//users, user
+// - ip, username
+//posts, post
+// - text, link, image
+//banner
+// - image, username
+//pinned
+// - text, username
+
+//post//
+//user
+//post
+//banner
+//pinnned
